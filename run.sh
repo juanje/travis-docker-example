578bba6a8518846b98fb176be52725627c0c9a97
Juanje Ojeda <juanje.ojeda@gmail.com>
2014-06-19 17:17:15 +0200

    Launch manually the docker daemon

--- run.sh
+++ run.sh
@@ -3,10 +3,19 @@
 # Exit on first error
 set -e
 
+echo "Linux distro:"
+lsb_release -a
+echo
+echo "Kernel version: $(uname -a)"
+
 # Wait for the Docker daemon to be running
-sleep 2
+docker -d &
+sleep 1
 
 # Use docker
 docker run busybox /bin/echo hello world
+status=$?
 
-exit $?
+kill $!
+
+exit $status
